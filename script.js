e2493f8ab4a3348888cd48fbe5be5dd9c5dc0e81
Liam Holland <91599612+liamholla@users.noreply.github.com>
2023-06-18 16:11:53 +0100

    Score is now kept this each click

--- script.js
+++ script.js
@@ -1,96 +1,114 @@
- //We are going to play a game of rock, paper, scissors with a computer
-        //Let's write out the steps
+//We are going to play a game of rock, paper, scissors with a computer
+//Let's write out the steps
 
-        //Set up the initial score for user and computer
-        let userScore = 0;
-        let computerScore = 0;
+//Set up the initial score for user and computer
+let userScore = 0;
+let computerScore = 0;
 
-    
-        //Take the user guess of R,P,S. We need to make this be based on a user click
-        //To use querySelectorAll we have to loop over all with the forEach method
+//Take the user guess of R,P,S. We need to make this be based on a user click
+//To use querySelectorAll we have to loop over all with the forEach method
 
-        const btnList = document.querySelectorAll('button');
-        
-        //The playRound function will be called when the user clicks a button
-        btnList.forEach(function(btn) {
-            btn.addEventListener('click', function(e) {
+const btnList = document.querySelectorAll("button");
 
-        // We can create a constant based on what the player clicks
-        console.log(e.target.innerHTML.toLowerCase());
-        const playerSelection = e.target.innerHTML.toLowerCase();
+//Define the roundWinner to use later on
+let roundWinner = "initial";
+console.log(roundWinner);
 
-        //Make a function that will play a round of RPS each time the users clicks
-        function playRound() {
+//Define a current score
+let currentScore = `USER 0 - 0 COMPUTER`;
+console.log(currentScore);
 
-            //Assign a random number between 1-3, this will be the computer guess and will
-            //act as a proxy for R,P,S.
-    
-            //Function to generate random integers between min and max
-            function getComputerChoice(min,max) {
-                min = Math.ceil(min);
-                max = Math.floor(max);
-                return Math.floor(Math.random() * (max - min + 1)) + min;
-            }
-            
-            //RPS only has 3 choices, this could be increased if we played RPS, lizard, spock for example
-            let min = 1;
-            let max = 3;
-    
-            //generate a random number between 1-3 and store as computer guess
-            let computerGuess = getComputerChoice(min,max)
-            
-            //Assign 1,2,3 and ROCK, PAPER, SCISSORS
-            let computerSelection ; 
-            switch(computerGuess) {
-                case 1:
-                    computerSelection = "rock";
-                    break;
-                case 2:
-                    computerSelection = "paper"
-                    break;
-                case 3:
-                    computerSelection = "scissors"   
-            }
-    
-            //Here we need to bring in the playerSelection constant that we made earlier
-            console.log(`Computer RPS = ${computerSelection}`)
-            console.log(`User RPS = ${playerSelection}`)
-    
-    
-            //If user = computer then DRAW
-            //If user = rock and computer = scissors then HUMAN WIN
-            //If user = rock and computer = paper then COMPUTER WIN
-            //If user = paper and computer = rock then HUMAN WIN
-            //If user = paper and computer = scissors then COMPUTER WIN
-            //If user = scissors and computer = paper then HUMAN WIN
-            //If user = scissors and computer = rock then COMPUTER WIN
-    
-            let roundWinner ;  if (playerSelection === computerSelection) {
-                roundWinner = "DRAW";
-            } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-                roundWinner =  "USER WIN";
-            } else if ((playerSelection === "rock") && (computerSelection === "paper")) {
-                roundWinner =  "COMPUTER WIN";
-            } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
-                roundWinner = "USER WIN";
-            } else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
-                roundWinner = "COMPUTER WIN";
-            } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-                roundWinner = "USER WIN";
-            } else if ((playerSelection === "scissors") && (computerSelection === "rock")) {
-                roundWinner = "COMPUTER WIN";
-            }
-    
-            return roundWinner;
-        }
-        
-        //We can log the winner of the round
-        console.log(playRound());
-            });
-          });
+//Whenever the user clicks a button, they will play 1 round
+btnList.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    // We can create a constant based on what the player clicks
+    console.log(e.target.innerHTML.toLowerCase());
+    const playerSelection = e.target.innerHTML.toLowerCase();
 
+    //Assign a random number between 1-3, this will be the computer guess and will
+    //act as a proxy for R,P,S.
 
-        /*
+    //Function to generate random integers between min and max
+    function getComputerChoice(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    //RPS only has 3 choices, this could be increased if we played RPS, lizard, spock for example
+    let min = 1;
+    let max = 3;
+
+    //generate a random number between 1-3 and store as computer guess
+    let computerGuess = getComputerChoice(min, max);
+
+    //Assign 1,2,3 and ROCK, PAPER, SCISSORS
+    let computerSelection;
+    switch (computerGuess) {
+      case 1:
+        computerSelection = "rock";
+        break;
+      case 2:
+        computerSelection = "paper";
+        break;
+      case 3:
+        computerSelection = "scissors";
+    }
+
+    //Here we need to bring in the playerSelection constant that we made earlier
+    console.log(`Computer RPS = ${computerSelection}`);
+    console.log(`User RPS = ${playerSelection}`);
+
+    //If user = computer then DRAW
+    //If user = rock and computer = scissors then HUMAN WIN
+    //If user = rock and computer = paper then COMPUTER WIN
+    //If user = paper and computer = rock then HUMAN WIN
+    //If user = paper and computer = scissors then COMPUTER WIN
+    //If user = scissors and computer = paper then HUMAN WIN
+    //If user = scissors and computer = rock then COMPUTER WIN
+
+    if (playerSelection === computerSelection) {
+      roundWinner = "DRAW";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+      roundWinner = "USER WIN";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+      roundWinner = "COMPUTER WIN";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+      roundWinner = "USER WIN";
+    } else if (
+      playerSelection === "paper" &&
+      computerSelection === "scissors"
+    ) {
+      roundWinner = "COMPUTER WIN";
+    } else if (
+      playerSelection === "scissors" &&
+      computerSelection === "paper"
+    ) {
+      roundWinner = "USER WIN";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      roundWinner = "COMPUTER WIN";
+    }
+
+    console.log(roundWinner);
+
+    //Make a function that will increase the score per round
+    function score() {
+      //After a win, add 1 to the score of either the user or computer
+      if (roundWinner === "USER WIN") {
+        userScore++;
+      }
+      if (roundWinner === "COMPUTER WIN") {
+        computerScore++;
+      }
+      return `USER ${userScore} - ${computerScore} COMPUTER`;
+    }
+
+    currentScore = score();
+    console.log(currentScore);
+  });
+});
+
+/*
 
         //Make a function that will increase the score per round
         function score() {
