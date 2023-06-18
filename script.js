13997ae48037c7dc131576cfaa9d8ef31192597e
Liam Holland <91599612+liamholla@users.noreply.github.com>
2023-06-18 20:14:41 +0100

    Working RPS game
    Improved the look of the webpage with CSS.
    
    Changed the prompt when either the computer or user gets to 5

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ let computerScore = 0;
 
 //Take the user guess of R,P,S. We need to make this be based on a user click
 //To use querySelectorAll we have to loop over all with the forEach method
-
 const btnList = document.querySelectorAll("button");
 
 //Define the roundWinner to use later on
@@ -18,12 +17,19 @@ console.log(roundWinner);
 let currentScore = `USER 0 - 0 COMPUTER`;
 console.log(currentScore);
 
+//Create a round number variable that tracks the number of 'successful' clicks
+let roundNumber = 0;
+
 //Whenever the user clicks a button, they will play 1 round
 btnList.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
+
+    roundNumber ++;
+
     // We can create a constant based on what the player clicks
     console.log(e.target.innerHTML.toLowerCase());
     const playerSelection = e.target.innerHTML.toLowerCase();
+
 
     //Assign a random number between 1-3, this will be the computer guess and will
     //act as a proxy for R,P,S.
@@ -103,8 +109,26 @@ btnList.forEach(function (btn) {
       return `USER ${userScore} - ${computerScore} COMPUTER`;
     }
 
+    // Create a current score variable
     currentScore = score();
     console.log(currentScore);
+
+    // Edit the text inside the currentScoreText
+    let scoreText = document.getElementById("currentScoreText");
+    scoreText.innerHTML = currentScore;
+
+    currentRoundPara = document.getElementById("currentRoundText")
+    currentRoundPara.innerHTML = `You have played <b>${roundNumber}</b> rounds`;
+
+    console.log(`You have played ${roundNumber} rounds`);
+
+    //Update the message when the user of computer gets to 5
+    if (userScore === 5) {
+      scoreText.innerHTML = "You win!";
+    }
+    else if (computerScore === 5) {
+      scoreText.innerHTML = "The computer wins!"
+    }
   });
 });
 
